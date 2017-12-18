38d4ac27c28b8298d99d025dc7863a0fa109417e
motoshimadaisuke <doragon111999111999@yahoo.co.jp>
2017-12-18 11:19:28 +0900

    [AI]AIの修正と追加

---
+++ Assets/Resources/motoshimadaisuke/MarkerDistance.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerDistance : BaseObject {
+    public float PlayerDistans;
+    [SerializeField]
+    private GameObject[] MarkerObject;
+    private int num, i;
+    public int MarkerCnt;
+    
+	// Use this for initialization
+	void Start () {
+        FindObject();
+
+    }
+
+    // Update is called once per frame
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        Distance();
+    }
+    void FindObject()//マーカーを発見しMarkerObjectに入れる
+    {
+        MarkerObject = new GameObject[num];
+        for (i = 0; i < num; i++)
+        {
+            MarkerObject[i] = GameObject.Find("HitMarker" + i);//+iでマーカーの番号を示して要素の数だけFindして見つける
+
+            if (MarkerObject[i] == null)
+            {
+                Debug.Log("null");
+                MarkerObject[i] = GameObject.Find("Center");
+            }
+            Debug.Log(MarkerObject[i].transform.position);
+        }
+    }
+    void Distance()
+    {
+        PlayerDistans = ((transform.position.x - MarkerObject[MarkerCnt].transform.position.x) + (transform.position.z - MarkerObject[MarkerCnt].transform.position.z));
+    }
+}
---
+++ Assets/Resources/motoshimadaisuke/RankManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankManager : BaseObject {
+    public int[] MarkerNum;
+    MarkerDistance markerDistance;
+    private GameObject[] EnemyStats;
+    public float[] Distance;
+    public int[] Rank= { 1, 1, 1, 1 };
+    private int PlayerNum;
+    // Use this for initialization
+    void Start () {
+       
+        
+    }
+
+    // Update is called once per frame
+    public override void OnUpdate()
+    {
+        base.OnUpdate();       
+        Status();
+        Judgment();
+    }
+    void Judgment()
+    {
+        for (int i = 0; i < PlayerNum; i++)
+        {
+
+            if (MarkerNum[i] < MarkerNum[i + 1])
+            {
+
+                Rank[i] += 1;
+                if (Rank[i + 1] > 1)
+                {
+                    Rank[i + 1] -= 1;
+                }
+            }
+            else if (MarkerNum[i] > MarkerNum[i + 1])
+            {
+                Rank[i + 1] += 1;
+                if (Rank[i] > 1)
+                {
+                    Rank[i] -= 1;
+                }
+            }
+            else if (MarkerNum[i] == MarkerNum[i + 1])
+            {
+                if (Distance[i] < Distance[i + 1])
+                {
+                    Rank[i] += 1;
+                    if (Rank[i + 1] > 1)
+                    {
+                        Rank[i + 1] -= 1;
+                    }
+                }
+                else if (Distance[i] > Distance[i + 1])
+                {
+                    Rank[i + 1] += 1;
+                    if (Rank[i] > 1)
+                    {
+                        Rank[i] -= 1;
+                    }
+                }
+            }
+        }
+        
+    }
+    void Status()
+    {
+        EnemyStats = new GameObject[PlayerNum];
+        MarkerNum = new int[PlayerNum];
+        Distance = new float[PlayerNum];
+        for(int k = 0; k < PlayerNum; k++)
+        {
+            EnemyStats[k] = GameObject.Find("Enemy");
+            if (EnemyStats == null)
+            {
+                EnemyStats[k] = GameObject.Find("Player");
+            }
+        }
+        
+        for (int i = 0; i < PlayerNum; i++)
+        {
+            markerDistance = EnemyStats[i].GetComponent<MarkerDistance>();
+            MarkerNum[i] = markerDistance.MarkerCnt;
+            Distance[i] =markerDistance.PlayerDistans;
+        }
+        
+    }
+}
