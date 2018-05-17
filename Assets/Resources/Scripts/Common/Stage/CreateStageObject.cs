f6f478781fc021731f07054d72c33033c55cedc6
nwuser <nwuser@da.iwasaki.ac.jp>
2018-05-17 14:21:30 +0900

    【#35】[StageCreate]　ステージ生成時にNullだった場合のバグ対策

--- Assets/Resources/Scripts/Common/Stage/CreateStageObject.cs
+++ Assets/Resources/Scripts/Common/Stage/CreateStageObject.cs
@@ -16,11 +16,30 @@ public class CreateStageObject : BaseObject
 
     [SerializeField]
     private GameObject[] StageObject; // 生成をしたいステージを格納する配列
-   
+
+    /// <summary>
+    /// @brief ステージの初期化関数
+    /// </summary>
+    private void StageInitialize()
+    {
+        GameObject[] StageObject = new GameObject[8];
+    }
+
+    /// <summary>
+    /// @brief ステージの生成関数
+    /// </summary>
+    /// <param name="eStageType"></param>
+    private void CreateStage(EStageType eStageType)
+    {
+        if(New(StageObject[(int)eStageType]) == null)
+        {
+            New(StageObject[(int)EStageType.TutorialStage_Sailing]);
+        }
+    }
+
     public void Start()
     {
-
-        New(StageObject[(int)GameInstance.Instance.StageType]);// 生成
-        
+        StageInitialize();
+        CreateStage(GameInstance.Instance.StageType);
     }
 }
