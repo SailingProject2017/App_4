2d077625467e6e6b59de76c6e584f96f804c3cfd
杉山　涼 <sugisugirr@sugiyama-no-MacBook-Airlocal.local>
2018-05-25 14:54:47 +0900

    【#26】[AI]
    かくつきの解消

--- Assets/Resources/Scripts/Common/Manager/GameManager.cs
+++ Assets/Resources/Scripts/Common/Manager/GameManager.cs
@@ -8,6 +8,32 @@ public class GameManager : BaseObject
     private bool _activeMode = false;
     private GameObject GameObj;
 
+    [SerializeField]
+    private float m_updateInterval = 0.5f;
+
+    private float m_accum;
+    private int m_frames;
+    private float m_timeleft;
+    private float m_fps;
+
+    private void FPS()
+    {
+        m_timeleft -= Time.deltaTime;
+        m_accum += Time.timeScale / Time.deltaTime;
+        m_frames++;
+
+        if (0 < m_timeleft) return;
+
+        m_fps = m_accum / m_frames;
+        m_timeleft = m_updateInterval;
+        m_accum = 0;
+        m_frames = 0;
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("FPS: " + m_fps.ToString("f2"));
+    }
 
     protected override void AppendListConstructor()
     {
@@ -16,7 +42,13 @@ public class GameManager : BaseObject
         AppendManagerObjectToList(this);
     }
 
-    public bool ActiveMode
+	public override void OnUpdate()
+	{
+		base.OnUpdate();
+		FPS();
+	}
+
+	public bool ActiveMode
     {
         get{ return _activeMode; }
         set { _activeMode = value; }
