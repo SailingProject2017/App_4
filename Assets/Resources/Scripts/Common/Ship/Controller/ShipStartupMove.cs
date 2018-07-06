10c6e99b715f5d491f9d083bfbee95199ac5e77f
yutatakatsu <vocaloid_asax@yahoo.co.jp>
2018-07-06 11:50:03 +0900

    【none】[stage]
    ステージにリザルトやブイを追加
    スタート時の処理を追加

--- Assets/Resources/Scripts/Common/Ship/Controller/ShipStartupMove.cs
+++ Assets/Resources/Scripts/Common/Ship/Controller/ShipStartupMove.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
+﻿/**********************************************************************************************/
+/*! @file     ShipStartupMove.cs
+*********************************************************************************************
+* @brief      InGameでの船の発進時のセットアップ
+*********************************************************************************************
+* @author     Yuta Takatsu
+*********************************************************************************************
+* Copyright © 2018 yuta takatsu All Rights Reserved.
+**********************************************************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipStartupMove : MonoBehaviour {
+public class ShipStartupMove : BaseObject {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private bool isCallOnce = false; // @brief 一度だけ呼ばれることを保証する
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+
+        if (!isCallOnce)
+        {
+            StartCoroutine(ShipSetup());
+            isCallOnce = !isCallOnce;
+        }
+
+    }
+
+
+    /// <summary>
+    /// @brief 読み込み中の空白時間を埋めるコルーチン
+    /// </summary>
+    public IEnumerator ShipSetup()
+    {
+
+        yield return new WaitForSeconds(5.0f);
+        BaseObjectSingleton<GameInstance>.Instance.IsCountDown = true;
+
+    }
 }
