b9cdee78ddd91219c8e4a5884cd90b24cdc0253d
reinasawai <animevokaro39@gmail.com>
2018-10-21 23:28:55 +0900

    【none】[InGame]
    風のスクリプトの作成途中のものです。

--- Assets/Resources/Scripts/Common/Wind/GetWindParam.cs
+++ Assets/Resources/Scripts/Common/Wind/GetWindParam.cs
@@ -9,20 +9,82 @@
 **********************************************************************************************/
 using System;
 using UnityEngine;
+using System.Collections;
 
 public class GetWindParam : BaseObject
 {
-    
-    // 0~360まで
-    [Range(0, 360)]
-    private float valueWind = 0;　   //@brief 風の方向
-
-    /// <summary>
-    /// @brief 例外処理
-    /// </summary>
-    private int random(object wind)
+    private float valueWind;    //@brief 風の方向
+    private float timeWind;     //@brief 風向きを変化させるタイマー
+    void Start()
     {
-        throw new NotImplementedException();
+        /*******************************************************
+          数値のランダム化は完了。
+          矢印との連携をさせる。
+          連携が終わり次第、何秒後かに数字を変化させる
+          数値で近いほうから周る
+         *******************************************************/
+        valueWind = 0;
+        valueWind += UnityEngine.Random.Range(-180,180);
+        Debug.Log(valueWind);
+
+        //360度にあわせて条件指定
+        if (valueWind==0)
+        {
+            transform.Rotate(new Vector3(0f, 0f, 0f)); // y軸を軸として90°回転
+            Debug.Log("0か360だな！!(=ﾟωﾟ)ﾉ");
+        }
+        if (valueWind > 1&&valueWind < 45)
+        {
+            transform.Rotate(new Vector3(0f, 45f, 0f));
+            Debug.Log("右上！( ﾟДﾟ)");
+        }
+        if (valueWind > 46 && valueWind < 90)
+        {
+            Debug.Log("右！！");
+            transform.Rotate(new Vector3(0f, 90f, 0f));
+        }
+        if (valueWind > 91 && valueWind < 135)
+        {
+            transform.Rotate(new Vector3(0f, 135f, 0f));
+            Debug.Log("右下！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！");
+        }
+        if (valueWind > 136 && valueWind < 180||valueWind>-180&&valueWind<-135)
+        {
+            transform.Rotate(new Vector3(0f, 180f, 0f));
+            Debug.Log("下！！！！！！！！！！！！！！！！！(ﾟ∀ﾟ)ｱﾋｬ");
+        }
+        //ここから指定してない
+        if (valueWind > 181 && valueWind < 225)
+        {
+            transform.Rotate(new Vector3(0f, 225f, 0f));
+            Debug.Log("左下aaaaaaaaaaaaa_(:3」∠)_");
+        }
+        if (valueWind > 226 && valueWind < 270)
+        {
+            transform.Rotate(new Vector3(0f, 270f, 0f));
+            Debug.Log("左＼(゜ロ＼)ココハドコ? (／ロ゜)／アタシハダアレ?");
+        }
+        if (valueWind > 271 && valueWind < 315)
+        {
+            transform.Rotate(new Vector3(0f, 315f, 0f));
+            Debug.Log("左上(´・ω・)");
+        }
+        if (valueWind > 316 && valueWind < 359)
+        {
+            transform.Rotate(new Vector3(0f, 0f, 0f));
+            Debug.Log("ｗｗｗっわああああああああああああああああああああああああ");
+        }
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        timeWind -= Time.deltaTime;
+        if (timeWind <= 0.0)
+        {
+            timeWind = 1.0f;
+        }
+
     }
 
     /// <summary>
@@ -34,18 +96,19 @@ public class GetWindParam : BaseObject
         set
         {
             // 風向きを0~360の中に指定する
-            if(value > 360)
+            if (value > 180)
             {
-                valueWind = value - 360;
+                valueWind = value - 180;
             }
-            else if (value < 0)
+            else if (value < -180)
             {
-                valueWind = 360 - value;
+                valueWind = 180 - value;
             }
             else
             {
                 valueWind = value;
             }
+
         }
     }
 }
---
+++ Assets/Resources/Scripts/Common/Wind/YajirusiRotate.cs
@@ -0,0 +1,26 @@
+﻿/*********************************************************************************************
+* @brief      yajorusiを動かす仮プログラム
+*********************************************************************************************
+* @author     Reina Sawai
+*********************************************************************************************
+* Copyright © 2018 Reina Sawai All Rights Reserved.
+**********************************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YajirusiRotate : BaseObject
+{
+
+    float minAngle = -10.0f;
+    float maxAngle = 180.0f;
+	public override void OnUpdate()
+    {
+        base.OnUpdate();
+        float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
+        //y軸に回転
+        transform.eulerAngles = new Vector3(0, angle, 0);
+    }
+	
+	
+}
