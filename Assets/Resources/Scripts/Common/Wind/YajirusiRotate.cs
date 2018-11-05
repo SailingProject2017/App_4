440c92cc405a0999f1f2c36742381ee28c026ae0
reinasawai <animevokaro39@gmail.com>
2018-11-06 00:32:17 +0900

    【#105】[InGame]
    風のUIのスクリプトです。

--- Assets/Resources/Scripts/Common/Wind/YajirusiRotate.cs
+++ Assets/Resources/Scripts/Common/Wind/YajirusiRotate.cs
@@ -11,16 +11,41 @@ using UnityEngine;
 
 public class YajirusiRotate : BaseObject
 {
+    private float timeWind; //@brief 風向きを変えるタイマー
+    
 
-    float minAngle = -10.0f;
+    float minAngle = 0.0f;
     float maxAngle = 180.0f;
-	public override void OnUpdate()
+
+   
+    public override void OnUpdate()
+      {
+          base.OnUpdate();
+          //-180から180に数字を制限
+          maxAngle += UnityEngine.Random.Range(-180, 180);
+
+          timeWind -= Time.deltaTime;
+
+
+
+        if (timeWind <= 0.0)
+         {
+             //5秒ごとに処理を行う
+            timeWind = 5.0f;
+            MoveStart();
+           
+          }
+      }
+
+    /// <summary>
+    /// 矢印を動かす関数
+    /// </summary>
+    void MoveStart()
     {
-        base.OnUpdate();
-        float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
-        //y軸に回転
-        transform.eulerAngles = new Vector3(0, angle, 0);
-    }
-	
-	
+            float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
+            //y軸に回転
+            transform.eulerAngles = new Vector3(0, angle, 0);
+        }
+
+    //Playerの傾きを取得して矢印もそれにそって角度を変える
 }
