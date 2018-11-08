65a8a0f6e7cc7b192a4c1df19427328fc4c0a30d
reinasawai <animevokaro39@gmail.com>
2018-11-08 15:37:59 +0900

    【#105】[InGame]
    風向きUIを作成しました。

--- Assets/Resources/Scripts/Common/Wind/YajirusiRotate.cs
+++ Assets/Resources/Scripts/Common/Wind/YajirusiRotate.cs
@@ -11,41 +11,19 @@ using UnityEngine;
 
 public class YajirusiRotate : BaseObject
 {
-    private float timeWind; //@brief 風向きを変えるタイマー
-    
+    [SerializeField]
+    private GameObject shipObj; // @brief 自分の船の情報
 
-    float minAngle = 0.0f;
-    float maxAngle = 180.0f;
+    [SerializeField]
+    private GetWindParam windParam; // @brief 風の情報
 
-   
     public override void OnUpdate()
-      {
-          base.OnUpdate();
-          //-180から180に数字を制限
-          maxAngle += UnityEngine.Random.Range(-180, 180);
-
-          timeWind -= Time.deltaTime;
-
-
-
-        if (timeWind <= 0.0)
-         {
-             //5秒ごとに処理を行う
-            timeWind = 5.0f;
-            MoveStart();
-           
-          }
-      }
-
-    /// <summary>
-    /// 矢印を動かす関数
-    /// </summary>
-    void MoveStart()
     {
-            float angle = Mathf.LerpAngle(minAngle, maxAngle, Time.time);
-            //y軸に回転
-            transform.eulerAngles = new Vector3(0, angle, 0);
-        }
+        base.OnUpdate();
 
-    //Playerの傾きを取得して矢印もそれにそって角度を変える
+        Debug.Log("回転");
+        // valueWindの値分回転
+        transform.eulerAngles = new Vector3(0, windParam.ValueWind - shipObj.transform.eulerAngles.y, 0);
+    }
 }
+   
